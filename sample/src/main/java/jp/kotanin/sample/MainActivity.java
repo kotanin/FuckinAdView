65099f04961dc60399bc981a727ef307331c7484
古谷雅勝 <MasakatsuKodani@toybox.local>
2017-07-23 16:06:17 +0900

    サンプルにListViewのスクロール時の対応も入れた

--- sample/src/main/java/jp/kotanin/sample/MainActivity.java
+++ sample/src/main/java/jp/kotanin/sample/MainActivity.java
@@ -2,13 +2,21 @@ package jp.kotanin.sample;
 
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import jp.kotanin.fuckinadview.FuckinAdView;
 
@@ -22,7 +30,32 @@ public class MainActivity extends AppCompatActivity {
         FuckinAdView fuckinAdView = (FuckinAdView)findViewById(R.id.fuckinAdView);
         fuckinAdView.setBackgroundColor(Color.RED);
 
+        ImageView iv = new ImageView(this);
+        iv.setImageDrawable(getDrawable(android.R.drawable.sym_def_app_icon));
+        FrameLayout.LayoutParams ivParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                Gravity.CENTER_HORIZONTAL
+        );
+        fuckinAdView.addView(iv, ivParams);
+
         findViewById(R.id.fuckinButton).setOnClickListener((v) -> fuckinAdView.fuckinAnimateGo());
         findViewById(R.id.fuckin0AlphaButton).setOnClickListener((v) -> fuckinAdView.setAlpha(0F));
+
+        ListView fuckinList = (ListView)findViewById(R.id.fuckinList);
+        List<Integer> numList = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) numList.add(i);
+        fuckinList.setAdapter(new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, numList));
+        fuckinList.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                fuckinAdView.fuckinAnimateGo();
+            }
+        });
     }
 }
